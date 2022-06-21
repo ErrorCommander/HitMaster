d9234128626f60e31c57fedd7d8adbf213ce859a
ErrorCommander <kai_412@mail.ru>
2022-06-22 01:44:02 +0300

    fixed error commit PullSystem

---
+++ Assets/Scripts/PoolSystem/Pool.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pool
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private int _initPoolSize;
+
+    public GameObject Tag => _prefab;
+    public int PoolSize => _listGO == null ? _initPoolSize : _listGO.Count;
+
+    private Queue<GameObject> _queueReadyGO;
+    private List<GameObject> _listGO;
+    private Transform _parentPool;
+    private ushort _objID = 0;
+
+    public Pool(GameObject prefab, int poolSize)
+    {
+        _prefab = prefab;
+        _initPoolSize = poolSize;
+    }
+
+    public void Initialize()
+    {
+        _queueReadyGO = new Queue<GameObject>();
+        _listGO = new List<GameObject>();
+        _parentPool = new GameObject("Pool: " + _prefab.name).transform;
+        _parentPool.transform.SetParent(Pooler.Instance.transform);
+        FillPool();
+    }
+
+    public void ChangePoolSize(int poolSize)
+    {
+        if (_queueReadyGO.Count >= poolSize)
+            return;
+
+        _initPoolSize = poolSize;
+        FillPool();
+    }
+
+    public GameObject TakeGameObject()
+    {
+        GameObject result;
+
+        if (_queueReadyGO.Count > 0)
+            result = _queueReadyGO.Dequeue();
+        else
+            result = AddGameObject();
+
+        result.SetActive(true);
+        return result;
+    }
+
+    public void ClearPool()
+    {
+        _initPoolSize = 0;
+        foreach (var go in _listGO)
+            MonoBehaviour.Destroy(go);
+
+        _queueReadyGO.Clear();
+        _listGO.Clear();
+        Debug.Log("Pool is Clean");
+    }
+
+    public void DestroyPool()
+    {
+        ClearPool();
+        MonoBehaviour.Destroy(_parentPool.gameObject);
+        Debug.Log("Pool destroyed");
+    }
+
+    private void FillPool()
+    {
+        if (_listGO.Count >= _initPoolSize)
+            return;
+
+        for (int i = _listGO.Count; i < _initPoolSize; i++)
+            AddGameObject();
+    }
+
+    private GameObject AddGameObject()
+    {
+        GameObject newObj = MonoBehaviour.Instantiate(_prefab, _parentPool);
+        newObj.SetActive(false);
+        newObj.name = string.Format("{0} {1:000}", _prefab.name, _objID++);
+        newObj.AddComponent<AutoEnqueue>().Initialize(_queueReadyGO);
+        _listGO.Add(newObj);
+        _queueReadyGO.Enqueue(newObj);
+
+        return newObj;
+    }
+}
---
+++ Assets/Scripts/PoolSystem/Pooler.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class Pooler : MonoBehaviour
+{
+    [SerializeField] private List<Pool> _pools;
+    private Dictionary<GameObject, Pool> _poolsDictionary;
+
+    public static Pooler Instance => _instance ??= new GameObject("Pooler GameObjects").AddComponent<Pooler>();
+    private static Pooler _instance;
+    private Pooler() { }
+
+    public bool AddPool(Pool pool)
+    {
+        if (_poolsDictionary.ContainsKey(pool.Tag))
+        {
+            if (_poolsDictionary[pool.Tag].PoolSize < pool.PoolSize)
+                _poolsDictionary[pool.Tag].ChangePoolSize(pool.PoolSize);
+            return false;
+        }
+
+        _poolsDictionary.Add(pool.Tag, pool);
+        _pools = _poolsDictionary.Values.ToList();
+        pool.Initialize();
+        return true;
+    }
+
+    public bool AddPool(Pool pool, GameObject poolGameObject)
+    {
+        pool = new Pool(poolGameObject, pool.PoolSize);
+        return AddPool(pool);
+    }
+
+    public GameObject[] GetTags()
+    {
+        return _poolsDictionary.Keys.ToArray();
+    }
+
+    public GameObject Spawn(GameObject tag, Vector3 position, Quaternion rotation)
+    {
+        if (!_poolsDictionary.ContainsKey(tag))
+            return null;
+
+        var pool = _poolsDictionary[tag];
+        var result = pool.TakeGameObject();
+        result.transform.SetPositionAndRotation(position, rotation);
+
+        return result;
+    }
+
+    public GameObject Spawn(GameObject tag, Vector3 position, Vector3 rotation)
+    {
+        return Spawn(tag, position, Quaternion.Euler(rotation));
+    }
+
+    public GameObject Spawn(GameObject tag, Vector3 position)
+    {
+        return Spawn(tag, position, Quaternion.identity);
+    }
+
+    public bool ClearPool(GameObject tag)
+    {
+        if (!_poolsDictionary.ContainsKey(tag))
+        {
+            Debug.Log("Tag not found");
+            return false;
+        }
+
+        Debug.Log("Pooler -> clear pool " + tag.name);
+        _poolsDictionary[tag].ClearPool();
+        return true;
+    }
+
+    public void ClearPool(params GameObject[] tags)
+    {
+        foreach (var tag in tags)
+            ClearPool(tag);
+    }
+
+    private void ClearAllPools()
+    {
+        if (_pools == null || _pools.Count == 0)
+            return;
+
+        ClearPool(GetTags());
+    }
+
+    public bool DestroyPool(GameObject tag)
+    {
+        if (!_poolsDictionary.ContainsKey(tag))
+        {
+            Debug.Log("Tag not found");
+            return false;
+        }
+
+        Debug.Log("Pooler -> Destroy pool " + tag.name);
+        _poolsDictionary[tag].DestroyPool();
+        _pools.Remove(_poolsDictionary[tag]);
+        _poolsDictionary.Remove(tag);
+        return true;
+    }
+
+    public void DestroyPool(params GameObject[] tags)
+    {
+        foreach (var tag in tags)
+            DestroyPool(tag);
+    }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _instance ??= this;
+        _poolsDictionary = new Dictionary<GameObject, Pool>();
+        _pools ??= new List<Pool>();
+        foreach (var pool in _pools)
+            AddPool(pool);
+
+        SceneManager.sceneUnloaded += UnloadScene;
+    }
+
+    private void DestroyAllPools()
+    {
+        if (_pools == null || _pools.Count == 0)
+            return;
+
+        DestroyPool(GetTags());
+        _poolsDictionary.Clear();
+        _pools.Clear();
+    }
+
+    private void OnDestroy()
+    {
+        DestroyAllPools();
+        Destroy(gameObject);
+    }
+
+    private void UnloadScene(Scene arg0)
+    {
+        _instance = null;
+        SceneManager.sceneUnloaded -= UnloadScene;
+    }
+}
