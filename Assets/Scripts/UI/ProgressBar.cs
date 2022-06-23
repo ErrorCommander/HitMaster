06ed4502e85d0dcc743e0a85333901ae15efcdb8
ErrorCommander <kai_412@mail.ru>
2022-06-23 16:30:45 +0300

    Add Animation in ProgressBar

--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ProgressBar : MonoBehaviour
 {
     [SerializeField] private Text _numberCheckPoint;
     [SerializeField] private Slider _progressInCheckPoint;
+    [SerializeField] [Range(0,5)] private float _animationSliderSpeed = 0.2f;
 
     private int _indexer = 0;
     private CheckPoint _currentCheckPoint;
+    private const float ReverseAnimationSpeed = 7f;
 
     private void Awake()
     {
@@ -21,12 +25,43 @@ public class ProgressBar : MonoBehaviour
     private void EnemyDie()
     {
         float progress = (_currentCheckPoint.EnemiesCount - _currentCheckPoint.LivingEnemiesCount) / (float)_currentCheckPoint.EnemiesCount;
+        StartCoroutine(AmimationProgressSlider(progress, _animationSliderSpeed));
+    }
+
+    private IEnumerator AmimationProgressSlider(float progress, float speed)
+    {
+        float value;
+        value = _progressInCheckPoint.normalizedValue + speed * Time.deltaTime;
+
+        while (value < progress)
+        {
+            _progressInCheckPoint.normalizedValue = value;
+            yield return null;
+            value = _progressInCheckPoint.normalizedValue + speed * Time.deltaTime;
+        }
+
         _progressInCheckPoint.normalizedValue = progress;
+    }
+
+    private IEnumerator AmimationProgressSliderToZero()
+    {
+        float value;
+        value = _progressInCheckPoint.normalizedValue - ReverseAnimationSpeed * Time.deltaTime;
+
+        while (value > 0)
+        {
+            _progressInCheckPoint.normalizedValue = value;
+            yield return null;
+            value = _progressInCheckPoint.normalizedValue - ReverseAnimationSpeed * Time.deltaTime;
+        }
+
+        _progressInCheckPoint.normalizedValue = 0;
     }
 
     private void ResetProgressSlider(CheckPoint point)
     {
-        _progressInCheckPoint.normalizedValue = 0;
+        StopAllCoroutines();
+        StartCoroutine(AmimationProgressSliderToZero());
         _currentCheckPoint = point;
     }
 
