c482ba4d0e299ccde963080c20f50778b451f0c5
ErrorCommander <kai_412@mail.ru>
2022-07-05 04:57:44 +0300

    Rename class

--- Assets/Scripts/Game Logic/FrendlyAnimIsSafe.cs
+++ Assets/Scripts/Game Logic/FrendlyAnimIsSafe.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IsSafeCheckPoint : MonoBehaviour
+public class FrendlyAnimIsSafe : MonoBehaviour
 {
     [SerializeField] private CheckPoint _checkPoint;
     [SerializeField] private List<Animator> _animators;
@@ -11,12 +11,12 @@ public class IsSafeCheckPoint : MonoBehaviour
 
     private void Start()
     {
-        _checkPoint.CheckPointPassed += SetInAnimators;
+        _checkPoint.CheckPointPassed += SafeAnimation;
     }
 
-    private void SetInAnimators()
+    private void SafeAnimation()
     {
-        _checkPoint.CheckPointPassed -= SetInAnimators;
+        _checkPoint.CheckPointPassed -= SafeAnimation;
         foreach (var amin in _animators)
         {
             amin.SetBool(_animVarIsSafe, true);
